c67bf092652a5161225e6208e3e7e6573273b44e
rute00 <88807596+rute00@users.noreply.github.com>
2022-03-27 02:20:29 -0300

    Delete Source-Code-Inspection-master/Source Code Inspection directory

--- Source-Code-Inspection-master/Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
+++
@@ -1,8 +0,0 @@
-//ATIVIDADE INSPEÇÃO DE CÓDIGO
-
-package br.calebe.ticketmachine.exception;
-
-
-public class PapelMoedaInvalidaException extends Exception {
-    
-}
--- Source-Code-Inspection-master/Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
+++
@@ -1,7 +0,0 @@
-//ATIVIDADE INSPEÇÃO DE CÓDIGO
-
-package br.calebe.ticketmachine.exception;
-
-
-public class SaldoInsuficienteException extends Exception {
-}
