c4038dc3bdbcd8eaa0f87655e9c2ff1be894dc55
rute00 <88807596+rute00@users.noreply.github.com>
2022-03-27 02:23:16 -0300

    Add files via upload

---
+++ Source-Code-Inspection-master/Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
@@ -0,0 +1,8 @@
+//ATIVIDADE INSPEÇÃO DE CÓDIGO
+
+package br.calebe.ticketmachine.exception;
+
+
+public class SaldoInsuficienteException extends Exception { 
+    //métodos não adicionados e declarados na classe - COMISSÃO 
+}
