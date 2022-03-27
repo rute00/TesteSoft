81ac77cfb03166ac1449d99c59a5f408240b7537
rute00 <88807596+rute00@users.noreply.github.com>
2022-03-27 01:38:46 -0300

    Add files via upload

---
+++ Source-Code-Inspection-master/Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
@@ -0,0 +1,8 @@
+//ATIVIDADE INSPEÇÃO DE CÓDIGO
+
+package br.calebe.ticketmachine.exception;
+
+
+public class PapelMoedaInvalidaException extends Exception {
+    
+}
---
+++ Source-Code-Inspection-master/Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
@@ -0,0 +1,7 @@
+//ATIVIDADE INSPEÇÃO DE CÓDIGO
+
+package br.calebe.ticketmachine.exception;
+
+
+public class SaldoInsuficienteException extends Exception {
+}
